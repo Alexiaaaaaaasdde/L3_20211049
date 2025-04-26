9a4c491765f1c0f688ab00afef33d5c47367d13d
AlexiaPT <153009848+Alexiaaaaaaasdde@users.noreply.github.com>
2025-04-25 20:41:56 -0500

    Initial commit

---
+++ src/main/java/com/example/l3_20211049/L320211049Application.java
@@ -0,0 +1,13 @@
+package com.example.l3_20211049;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L320211049Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L320211049Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/l3_20211049/L320211049ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l3_20211049;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L320211049ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
